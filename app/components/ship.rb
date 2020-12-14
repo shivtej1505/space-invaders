4d4066310cc9ac248c30a50486bda8fdd134209f
Shivang Nagaria <shivang.nagaria@gmail.com>
2020-12-15 00:08:16 +0530

    Basic ship controls added

--- app/components/ship.rb
+++ app/components/ship.rb
@@ -1,5 +1,27 @@
 class Ship
   def initialize
+    @image = Gosu::Image.new("app/assets/images/rocket.png")
+    @x = @y = 240
+    @vel_x = @vel_y = 1
+  end
 
+  def move_left
+    @x -= @vel_x
+  end
+
+  def move_right
+    @x += @vel_x
+  end
+
+  def move_up
+    @y -= @vel_y
+  end
+
+  def move_down
+    @y += @vel_y
+  end
+
+  def draw
+    @image.draw(@x, @y, 1)
   end
 end
--- app/game.rb
+++ app/game.rb
@@ -1,16 +1,27 @@
 require 'gosu'
+require_relative 'components/ship'
 
 class Game < Gosu::Window
   def initialize
     super(640, 480)
     self.caption = "Space Invaders"
+
+    @ship = Ship.new
   end
 
   def update
-    # ...
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+      @ship.move_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+      @ship.move_right
+    end
+    if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+      @ship.move_up
+    end
   end
 
   def draw
-    # ...
+    @ship.draw
   end
 end
