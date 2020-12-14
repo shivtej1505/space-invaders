5c8ff40aca62932b66d483d91f5c839b9282a523
Shivang Nagaria <shivang.nagaria@gmail.com>
2020-12-15 00:49:05 +0530

    Basic invader group

--- app/components/invader.rb
+++ app/components/invader.rb
@@ -1,5 +1,18 @@
 class Invader
-  def initialize
+  def initialize(x: 0.0, y: 0.0)
+    @image = Gosu::Image.new("app/assets/images/invader.png")
+    @x = x
+    @y = y
+    @vel_x = 1
+    @vel_y = 0.0
+  end
 
+  def move
+    @x += @vel_x
+    @y += @vel_y
+  end
+
+  def draw
+    @image.draw(@x, @y, 1)
   end
 end
--- app/game.rb
+++ app/game.rb
@@ -1,5 +1,13 @@
 require 'gosu'
 require_relative 'components/ship'
+require_relative 'components/invader'
+
+# TODO:
+# 1. Implement rows & movements of invaders
+# 2. Implement missile launch system from ship
+# 3. Implement missile launch system from invaders
+# 4. Score system
+# 5. Game start/pause/end system
 
 class Game < Gosu::Window
   def initialize
@@ -7,6 +15,7 @@ class Game < Gosu::Window
     self.caption = "Space Invaders"
 
     @ship = Ship.new
+    @invaders = 1.upto(10).map {|num| Invader.new(x: num*30) }
   end
 
   def update
@@ -19,9 +28,20 @@ class Game < Gosu::Window
     if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
       @ship.move_up
     end
+
+    if Gosu.button_down? Gosu::KB_ESCAPE
+      exit
+    end
+
+    @invaders.each do |invader|
+      invader.move
+    end
   end
 
   def draw
     @ship.draw
+    @invaders.each do |invader|
+      invader.draw
+    end
   end
 end
