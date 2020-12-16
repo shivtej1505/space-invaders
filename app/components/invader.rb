ac4a361e6cd5346346791872bb1d0dee98e1a800
Shivang Nagaria <shivang.nagaria@gmail.com>
2020-12-16 23:27:25 +0530

    Invader row movement added

--- app/components/invader.rb
+++ app/components/invader.rb
@@ -5,13 +5,21 @@ class Invader
     @image = Gosu::Image.new('app/assets/images/invader.png')
     @x = x
     @y = y
-    @vel_x = 1
+    @vel_x = 10
     @vel_y = 0.0
   end
 
   def move
     @x += @vel_x
     @y += @vel_y
+  end
+
+  def reverse_x
+    @vel_x = -@vel_x
+  end
+
+  def move_y
+    @y += 20
   end
 
   def fire
--- app/game.rb
+++ app/game.rb
@@ -7,18 +7,24 @@ require_relative 'components/invader'
 # TODO:
 # 1. Implement rows & movements of invaders
 # 2. Implement missile launch system from ship - done
-# 3. Implement missile launch system from invaders
+# 3. Implement missile launch system from invaders - done
 # 4. Score system
 # 5. Game start/pause/end system
 # 6. collision detection
 
 class Game < Gosu::Window
   def initialize
-    super(640, 480)
+    super(640, 480, false, 0.05)
     self.caption = 'Space Invaders'
 
     @ship = Ship.new
-    @invaders = 1.upto(10).map { |num| Invader.new(x: num * 30) }
+
+    @invaders = 1.upto(5).map { {vel_x: 10, x: 0, y: 0, invaders: []} }
+
+    @invaders.each_with_index do |invader_row, idx|
+      invader_row[:invaders] = 1.upto(10).map { |num| Invader.new(x: num * 30, y: idx * 30) }
+    end
+
     @missiles = []
     @paused = false
     @font = Gosu::Font.new(20)
@@ -42,7 +48,17 @@ class Game < Gosu::Window
       #   end
       # end
 
-      @invaders.each(&:move)
+      @invaders.each do |invader_row|
+        invader_row[:invaders].each(&:move)
+        invader_row[:x] += invader_row[:vel_x]
+        if invader_row[:x] > 480 || invader_row[:x] < 0
+          invader_row[:invaders].each(&:reverse_x)
+          invader_row[:vel_x] = -invader_row[:vel_x]
+          if invader_row[:x] < 0
+            invader_row[:invaders].each(&:move_y)
+          end
+        end
+      end
 
       @missiles.each(&:move)
     end
@@ -53,7 +69,9 @@ class Game < Gosu::Window
       @font.draw_text('Paused', 100, 100, 1)
     else
       @ship.draw
-      @invaders.each(&:draw)
+      @invaders.each do |invader_row|
+        invader_row[:invaders].each(&:draw)
+      end
 
       @missiles.each(&:draw)
     end
